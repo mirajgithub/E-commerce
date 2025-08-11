1585f538d56e7a98e1eb9ae3a1a4304996c4bf0e
Miraj Home <mirajugaming001@gmail.com>
2025-08-11 22:17:24 +0530

    update Api is done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import User from '../models/user.model.js'
-
+import bcrypt from 'bcryptjs';
 export const getUserdetails = async (req, res, next) => {
     try {
         const user = req.user;
@@ -18,16 +18,35 @@ export const getUserdetails = async (req, res, next) => {
     }
 }
 
-export const updateUserdetails = async (req, res, next) => {
+export const updateUserDetails = async (req, res, next) => {
     try {
-        const user = req.user;
+        const user = await User.findById(req.user._id);
         if (!user) {
-            res.status(404);
-            throw new Error('User not found');
+            return res.status(404).json({ message: 'User not found' });
         }
-        const userUpdate = User.findByIdAndUpdate(user._id);
 
+        // Whitelist of fields user is allowed to update
+        const allowedFields = ['first_name', 'last_name', 'email', 'phone'];
+
+        allowedFields.forEach(field => {
+            if (req.body[field] !== undefined) {
+                user[field] = req.body[field];
+            }
+        });
+
+        // If password is provided, hash it
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(req.body.password, salt);
+        }
+
+        await user.save();
+
+        res.status(200).json({
+            message: 'User updated successfully',
+            user
+        });
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUserdetails } from '../controllers/user.controller.js'
+import { getUserdetails, updateUserDetails } from '../controllers/user.controller.js'
 import { protect } from "../middlewares/authMiddlewares.js";
 const router = express.Router();
 
 router.get('/profile', protect, getUserdetails);
+router.post('/updateuser', protect, updateUserDetails);
 
 export default router;
